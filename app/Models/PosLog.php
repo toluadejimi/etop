be6197771a91aaab4c024ae0b5d3f85f875e6860
Jimmy <toluadejimi@gmail.com>
2024-03-02 15:13:27 +0100

    index adjusted

--- app/Models/PosLog.php
+++ app/Models/PosLog.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class PosLog extends Model
 {
     use HasFactory;
+
+
+
+    protected $casts = [
+        'accountBalance' => 'string',
+        'amount' => 'string',
+        'log_status' => 'string',
+
+
+    ];
+
+
+
 }
