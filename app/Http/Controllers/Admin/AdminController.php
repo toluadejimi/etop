c88b079564fe1768de0b10cad92de818ffa22c62
Jimmy <toluadejimi@gmail.com>
2024-04-09 13:57:50 +0100

    store data controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -9,7 +9,6 @@ use App\Models\User;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
@@ -140,130 +139,9 @@ class AdminController extends Controller
     }
 
 
-        public function create_terminal(request $request)
+    public function create_terminal(request $request)
     {
 
-        if(Auth::user()->role != 1 || Auth::user()->role != 2 ){
-
-            return response()->json([
-                'status' => false,
-                'message' => "You dont have permission to create a terminal"
-            ], 422);
-
-        }
-
-        if($request->user_id == null || $request->serialNumber == null){
-
-            return response()->json([
-                'status' => false,
-                'message' => "User ID or Serial Number can not be null"
-            ], 422);
-
-        }
-
-
-        $ter = Terminal::where('serialNumber', $request->serialNumber)->first() ?? null;
-        if ($ter == null) {
-
-            $term = new Terminal();
-            $term->tid = $request->tid;
-            $term->user_id = $request->user_id;
-            $term->ip = $request->ip;
-            $term->port = $request->port;
-            $term->ssl = $request->ssl;
-            $term->compKey1 = $request->compKey1;
-            $term->compKey2 = $request->compKey2;
-            $term->baseUrl = $request->baseurl;
-            $term->logoUrl = $request->logoUrl;
-            $term->serialNumber = $request->serialNumber;
-            $term->merchantName = $request->merchantName;
-            $term->mid = $request->mid;
-            $term->merchantaddress = $request->merchantaddress;
-            $term->pin = Hash::make($request->pin);
-            $term->save();
-
-
-            try {
-
-                $curl = curl_init();
-                $data = array(
-
-                    'tid'  => $request->tid,
-                    'user_id'  => $request->user_id,
-                    'ip'  => $request->ip,
-                    'port'  => $request->port,
-                    'ssl'  => $request->ssl,
-                    'compKey1'  => $request->compKey1,
-                    'compKey2'  => $request->compKey2,
-                    'baseUrl'  => $request->baseurl,
-                    'logoUrl'  => $request->logoUrl,
-                    'serialNumber'  => $request->serialNumber,
-                    'merchantName'  => $request->merchantName,
-                    'mid'  => $request->mid,
-                    'merchantaddress'  => $request->merchantaddress,
-                    'pin'  => Hash::make($request->pin),
-
-                );
-                $post_data = json_encode($data);
-
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => 'https://etopmerchant.com/api/store-terminal',
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'POST',
-                    CURLOPT_POSTFIELDS => $post_data,
-                    CURLOPT_HTTPHEADER => array(
-                        'Content-Type: application/json'
-                    ),
-                ));
-
-                $var = curl_exec($curl);
-                curl_close($curl);
-
-
-            } catch (QueryException $e) {
-                echo "$e";
-            }
-
-
-
-            return response()->json([
-                'status' => true,
-                'message' => "Terminal has been created successfully"
-            ], 200);
-
-        }
-
-
-        if ($ter != null) {
-
-            return response()->json([
-                'status' => false,
-                'message' => "Terminal Account Already  Exist"
-            ], 422);
-
-
-        }
-
-    }
-
-
-
-    public function update_terminal(request $request)
-    {
-
-        if (Auth::user()->role != 1 || Auth::user()->role != 2) {
-
-            return response()->json([
-                'status' => false,
-                'message' => "You dont have permission to create a terminal"
-            ], 422);
-
-        }
 
         if ($request->user_id == null || $request->serialNumber == null) {
 
@@ -274,65 +152,185 @@ class AdminController extends Controller
 
         }
 
-        $ter = Terminal::where('serialNumber', $request->serialNumber)->first() ?? null;
-        if($ter != null){
 
-            Terminal::where('serialNumber', $request->serialNumber)->update(['user_id' => $request->user_id]);
-
-            try {
-
-                $curl = curl_init();
-                $data = array(
-                    'user_id'  => $request->user_id,
-                    'serialNumber'  => $request->serialNumber,
-                );
-                $post_data = json_encode($data);
-
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => 'https://etopmerchant.com/api/update-terminal',
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'POST',
-                    CURLOPT_POSTFIELDS => $post_data,
-                    CURLOPT_HTTPHEADER => array(
-                        'Content-Type: application/json'
-                    ),
-                ));
-
-                $var = curl_exec($curl);
-                curl_close($curl);
+        if (Auth::user()->role == 1 || Auth::user()->role == 2) {
 
 
-            } catch (QueryException $e) {
-                echo "$e";
+            $ter = Terminal::where('serialNumber', $request->serialNumber)->first() ?? null;
+            if ($ter == null) {
+
+                $term = new Terminal();
+                $term->tid = $request->tid;
+                $term->user_id = $request->user_id;
+                $term->ip = $request->ip;
+                $term->port = $request->port;
+                $term->ssl = $request->ssl;
+                $term->compKey1 = $request->compKey1;
+                $term->compKey2 = $request->compKey2;
+                $term->baseUrl = $request->baseurl;
+                $term->logoUrl = $request->logoUrl;
+                $term->serialNumber = $request->serialNumber;
+                $term->merchantName = $request->merchantName;
+                $term->mid = $request->mid;
+                $term->merchantaddress = $request->merchantaddress;
+                $term->pin = Hash::make($request->pin);
+                $term->save();
+
+
+                try {
+
+                    $curl = curl_init();
+                    $data = array(
+
+                        'tid' => $request->tid,
+                        'user_id' => $request->user_id,
+                        'ip' => $request->ip,
+                        'port' => $request->port,
+                        'ssl' => $request->ssl,
+                        'compKey1' => $request->compKey1,
+                        'compKey2' => $request->compKey2,
+                        'baseUrl' => $request->baseurl,
+                        'logoUrl' => $request->logoUrl,
+                        'serialNumber' => $request->serialNumber,
+                        'merchantName' => $request->merchantName,
+                        'mid' => $request->mid,
+                        'merchantaddress' => $request->merchantaddress,
+                        'pin' => Hash::make($request->pin),
+
+                    );
+                    $post_data = json_encode($data);
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://etopmerchant.com/api/store-terminal',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => $post_data,
+                        CURLOPT_HTTPHEADER => array(
+                            'Content-Type: application/json'
+                        ),
+                    ));
+
+                    $var = curl_exec($curl);
+                    curl_close($curl);
+
+
+                } catch (QueryException $e) {
+                    echo "$e";
+                }
+
+
+                return response()->json([
+                    'status' => true,
+                    'message' => "Terminal has been created successfully"
+                ], 200);
+
             }
 
-            return response()->json([
-                'status' => true,
-                'message' => "User has been successfully attached"
-            ], 200);
 
-        }else{
+            if ($ter != null) {
+
+                return response()->json([
+                    'status' => false,
+                    'message' => "Terminal Account Already  Exist"
+                ], 422);
+
+
+            }
+
+        } else {
 
             return response()->json([
                 'status' => false,
-                'message' => "Terminal Not Found"
+                'message' => "You dont have permission to create a terminal"
             ], 422);
-
-
         }
-
-
-
     }
 
 
+    public function update_terminal(request $request)
+    {
 
-        public function get_all_transactions(request $request)
+
+        if ($request->user_id == null || $request->serialNumber == null) {
+
+            return response()->json([
+                'status' => false,
+                'message' => "User ID or Serial Number can not be null"
+            ], 422);
+
+        }
+
+        if (Auth::user()->role == 1 || Auth::user()->role == 2) {
+
+            $ter = Terminal::where('serialNumber', $request->serialNumber)->first() ?? null;
+            if ($ter != null) {
+
+                Terminal::where('serialNumber', $request->serialNumber)->update(['user_id' => $request->user_id]);
+
+                try {
+
+                    $curl = curl_init();
+                    $data = array(
+                        'user_id' => $request->user_id,
+                        'serialNumber' => $request->serialNumber,
+                    );
+                    $post_data = json_encode($data);
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://etopmerchant.com/api/update-terminal',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => $post_data,
+                        CURLOPT_HTTPHEADER => array(
+                            'Content-Type: application/json'
+                        ),
+                    ));
+
+                    $var = curl_exec($curl);
+                    curl_close($curl);
+
+
+                } catch (QueryException $e) {
+                    echo "$e";
+                }
+
+                return response()->json([
+                    'status' => true,
+                    'message' => "User has been successfully attached"
+                ], 200);
+
+            } else {
+
+                return response()->json([
+                    'status' => false,
+                    'message' => "Terminal Not Found"
+                ], 422);
+
+            }
+
+
+        } else {
+
+            return response()->json([
+                'status' => false,
+                'message' => "You dont have permission to update a terminal"
+            ], 422);
+        }
+    }
+
+
+    public
+    function get_all_transactions(request $request)
     {
         $get_all_transaction = PosLog::latest()->where('user_id', Auth::id())->get();
         return response()->json([
@@ -343,7 +341,8 @@ class AdminController extends Controller
     }
 
 
-    public function get_logged_data(request $request)
+    public
+    function get_logged_data(request $request)
     {
 
         $SerialNo = $request->header('serialnumber');
@@ -374,7 +373,8 @@ class AdminController extends Controller
 
     }
 
-    public function get_all_by_serial_logged_data(request $request)
+    public
+    function get_all_by_serial_logged_data(request $request)
     {
 
         $SerialNo = $request->header('serialnumber');
@@ -400,7 +400,8 @@ class AdminController extends Controller
     }
 
 
-    public function get_all_transaction_by_filter(request $request)
+    public
+    function get_all_transaction_by_filter(request $request)
     {
 
 
