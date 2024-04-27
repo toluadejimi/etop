f49a790bc65c9c5121d4494b0d784f6181a0213d
Jimmy <toluadejimi@gmail.com>
2024-04-27 18:01:47 +0100

    added export

--- app/Http/Controllers/Admin/BankController.php
+++ app/Http/Controllers/Admin/BankController.php
@@ -23,12 +23,75 @@ class BankController extends Controller
                 $request->photo->move(public_path('upload/image'), $fileName);
                 $file_url = url('') . "/public/upload/image/$fileName";
 
+                $bank = new Bank();
+                $bank->name = $request->name;
+                $bank->email = $request->email;
+                $bank->image = $file_url;
+                $bank->save();
+
+
+                $usr = new User();
+                $usr->first_name = $request->name;
+                $usr->last_name = $request->name;
+                $usr->email = $request->email;
+                $usr->role = 3;
+                $usr->bank_id = $bank->id;
+                $usr->password = bcrypt('123456');
+                $usr->save();
+
+
+                try {
+
+                    $curl = curl_init();
+                    $data = array(
+                        'email' => $request->email,
+                        'name' => $request->name,
+                        'image' => $file_url
+
+                    );
+                    $post_data = json_encode($data);
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://etopmerchant.com/api/create-bank',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => $post_data,
+                        CURLOPT_HTTPHEADER => array(
+                            'Content-Type: application/json'
+                        ),
+                    ));
+
+                    $var = curl_exec($curl);
+                    curl_close($curl);
+
+
+                } catch (QueryException $e) {
+                    echo "$e";
+                }
+
+                return response()->json([
+                    'status' => true,
+                    'data' => $bank,
+                    'message' => "Bank Created Successfully "
+                ], 200);
+
+            } else {
+                return response()->json([
+                    'status' => true,
+                    'message' => "You don't have permission"
+                ], 422);
+
             }
+
 
             $bank = new Bank();
             $bank->name = $request->name;
             $bank->email = $request->email;
-            $bank->image = $file_url;
             $bank->save();
 
 
@@ -42,14 +105,12 @@ class BankController extends Controller
             $usr->save();
 
 
-
             try {
 
                 $curl = curl_init();
                 $data = array(
                     'email' => $request->email,
                     'name' => $request->name,
-                    'image' => $file_url
 
                 );
                 $post_data = json_encode($data);
@@ -83,12 +144,10 @@ class BankController extends Controller
                 'message' => "Bank Created Successfully "
             ], 200);
 
-        }else{
+        } else {
 
-            return response()->json([
-                'status' => true,
-                'message' => "You don't have permission"
-            ], 422);
+            return response()->json(['status' => true,
+                'message' => "You don't have permission"], 422);
 
         }
     }
@@ -105,13 +164,61 @@ class BankController extends Controller
                 $request->photo->move(public_path('upload/image'), $fileName);
                 $file_url = url('') . "/public/upload/image/$fileName";
 
+                Bank::where('id', $request->id)->update([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'image' => $file_url,
+                ]);
+
+
+                try {
+
+                    $curl = curl_init();
+                    $data = array(
+                        'email' => $request->email,
+                        'name' => $request->name,
+                        'image' => $file_url,
+
+
+                    );
+                    $post_data = json_encode($data);
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://etopmerchant.com/api/update-bank',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => $post_data,
+                        CURLOPT_HTTPHEADER => array(
+                            'Content-Type: application/json'
+                        ),
+                    ));
+
+                    $var = curl_exec($curl);
+                    curl_close($curl);
+
+
+                } catch (QueryException $e) {
+                    echo "$e";
+                }
+
+
+                return response()->json([
+                    'status' => true,
+                    'message' => "Bank updated successfully "
+                ], 200);
+
             }
+
 
             Bank::where('id', $request->id)->update([
 
                 'name' => $request->name,
                 'email' => $request->email,
-                'image' => $file_url,
 
             ]);
 
@@ -122,7 +229,6 @@ class BankController extends Controller
                 $data = array(
                     'email' => $request->email,
                     'name' => $request->name,
-                    'image' => $file_url,
 
 
                 );
@@ -157,7 +263,7 @@ class BankController extends Controller
                 'message' => "Bank updated successfully "
             ], 200);
 
-        }else{
+        } else {
 
             return response()->json([
                 'status' => true,
@@ -168,7 +274,8 @@ class BankController extends Controller
     }
 
 
-    public function search_bank(request $request)
+    public
+    function search_bank(request $request)
     {
 
         if (Auth::user()->role == 1 || Auth::user()->role == 2) {
@@ -184,7 +291,6 @@ class BankController extends Controller
             ], 200);
 
 
-
         } else {
 
             return response()->json([
@@ -195,8 +301,8 @@ class BankController extends Controller
     }
 
 
-
-    public function delete_bank(request $request)
+    public
+    function delete_bank(request $request)
     {
         if (Auth::user()->role == 1 || Auth::user()->role == 2) {
 
@@ -207,7 +313,7 @@ class BankController extends Controller
                 'message' => "Bank updated successfully "
             ], 200);
 
-        }else{
+        } else {
 
             return response()->json([
                 'status' => true,
