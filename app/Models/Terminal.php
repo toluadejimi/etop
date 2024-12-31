d875f91e74c755a9041ea12d03a79c95c4475796
Jimmy <toluadejimi@gmail.com>
2024-12-31 20:51:16 +0100

    export terminal by  banks added

--- app/Models/Terminal.php
+++ app/Models/Terminal.php
@@ -39,4 +39,9 @@ class Terminal extends Model
     public $timestamps = false;
 
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function terminal()
+    {
+        return $this->hasMany(Terminal::class);
+    }
+
 }
