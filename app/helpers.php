9e26bc24184f5cec5d2a0d0701aa9f48b2922c2f
Jimmy <toluadejimi@gmail.com>
2025-01-03 15:51:41 +0100

    export terminal by  banks added

--- app/helpers.php
+++ app/helpers.php
@@ -119,7 +119,7 @@ if (!function_exists('send_notification')) {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.telegram.org/bot7317523938:AAFCcfuc-Zvsn9gkWC4fWyR7fxH0s2_kLGw/sendMessage?chat_id=1316552414',
+            CURLOPT_URL => 'https://api.telegram.org/bot7317523938:8194613340:AAE6p179L1bim6PThWNjKq9cWS1P8O5X_Qk',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
